f6dd0a032487619aad1efcca05c4181085f2091d
pa596bu500 <pa596bu500@gmail.com>
2026-01-05 22:32:15 +0800

    scope enum update

--- src/main/java/org/pabuff/dto/PagScopeTypeEnum.java
+++ src/main/java/org/pabuff/dto/PagScopeTypeEnum.java
@@ -1,11 +1,62 @@
 package org.pabuff.dto;
 
 public enum PagScopeTypeEnum {
-    location,
-    locationGroup,
-    building,
-    site,
-    siteGroup,
-    project,
-    none,
+    location("location", "Location", "loc"),
+    locationGroup("location_group", "Location Group", "lg"),
+    building("building", "Building", "b"),
+    site("site", "Site", "s"),
+    siteGroup("site_group", "Site Group", "sg"),
+    project("project", "Project", "p"),
+    none("none", "None", "n"),;
+
+    private final String value;
+    private final String label;
+    private final String tag;
+
+    PagScopeTypeEnum(String value, String label, String tag) {
+        this.value = value;
+        this.label = label;
+        this.tag = tag;
+    }
+
+    // Getters
+    public String getValue() {
+        return value;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    // Reverse lookup by value
+    public static PagScopeTypeEnum fromValue(String value) {
+        for (PagScopeTypeEnum status : PagScopeTypeEnum.values()) {
+            if (status.value.equalsIgnoreCase(value)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown value: " + value);
+    }
+
+    public static PagScopeTypeEnum fromTag(String tag) {
+        for (PagScopeTypeEnum status : PagScopeTypeEnum.values()) {
+            if (status.tag.equalsIgnoreCase(tag)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown tag: " + tag);
+    }
+
+    public static PagScopeTypeEnum fromLabel(String label) {
+        for (PagScopeTypeEnum status : PagScopeTypeEnum.values()) {
+            if (status.label.equalsIgnoreCase(label)) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("Unknown label: " + label);
+    }
 }
